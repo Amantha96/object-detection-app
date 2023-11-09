6f030fa6b62f0338dea7d63cc6c039854443a527
Amantha96 <amanthabandara1996@gmail.com>
2023-11-09 23:33:57 +0530

    Updated code

--- src/components/MiddleBox/Middlebox.jsx
+++ src/components/MiddleBox/Middlebox.jsx
@@ -4,32 +4,61 @@ import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import { ref, uploadBytes } from "firebase/storage";
 import { v4 } from 'uuid';
 import { imageDB } from '../../firebase/config';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function MiddleBox() {
     const [img, setImg] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
     const fileInputRef = useRef(null);
+
+    const [buttonText, setButtonText] = useState("UPLOAD");
+
+    // State to toggle whether the yellowbox image is displayed
+    const [isYellowboxImageDisplayed, setIsYellowboxImageDisplayed] = useState(false);
 
     const handleUpload = () => {
         if (img) {
+            setIsLoading(true);
             const imgRef = ref(imageDB, `files/${v4()}`);
             uploadBytes(imgRef, img)
                 .then(() => {
-                    console.log('Image uploaded successfully.');
+                    alert('Image uploaded successfully');
+                    setImg(null);
+                    setButtonText("TRACK");
+                    setIsYellowboxImageDisplayed(false); // Reset the yellowbox image display
+                    setIsLoading(false);
                 })
                 .catch((error) => {
                     console.error('Error uploading image:', error);
+                    setIsLoading(false);
                 });
         } else {
-            console.error('No image selected for upload.');
+            console.error('No image selected for upload');
         }
     }
 
+
     const handleFileChange = (event) => {
-        setImg(event.target.files[0]);
+        const selectedImage = event.target.files[0];
+        setImg(selectedImage);
+
+        if (selectedImage) {
+            const yellowbox = document.querySelector('.yellowbox');
+            yellowbox.style.backgroundImage = `url(${URL.createObjectURL(selectedImage)}`;
+            yellowbox.style.backgroundSize = 'cover';
+            setIsYellowboxImageDisplayed(true); // Set the yellowbox image display to true
+        }
     }
 
     const openFileInput = () => {
         fileInputRef.current.click();
+    }
+
+    // Function to reset the yellowbox image
+    const resetYellowbox = () => {
+        const yellowbox = document.querySelector('.yellowbox');
+        yellowbox.style.backgroundImage = 'none';
+        setIsYellowboxImageDisplayed(false); // Reset the yellowbox image display
     }
 
     return (
@@ -39,21 +68,30 @@ function MiddleBox() {
                     <h1>Your photos</h1>
                     <p>Take photos or add existing ones from the gallery</p>
                 </div>
-                <div className="yellowbox" onClick={openFileInput}>
+                <div className="yellowbox" onClick={isYellowboxImageDisplayed ? resetYellowbox : openFileInput}>
                     <input
                         type="file"
                         ref={fileInputRef}
                         style={{ display: 'none' }}
                         onChange={handleFileChange}
                     />
-                    <AddAPhotoIcon style={{
-                        fontSize: '70px',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }} />
+                    {isLoading ? (
+                        <div className="loading-indicator">
+                            <CircularProgress style={{ color: 'black' }} />
+                        </div>
+                    ) : (
+                        (buttonText !== "TRACK" && !img) ? (
+                            <AddAPhotoIcon style={{
+                                fontSize: '70px',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center'
+                            }} />
+                        ) : null
+                    )}
                 </div>
-                <button onClick={handleUpload} className="uploadbtn">UPLOAD</button>
+
+                <button onClick={handleUpload} className="uploadbtn">{buttonText}</button>
             </div>
         </div>
     );
