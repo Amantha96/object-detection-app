4c67d6dfb42a7d9f1aec5b7d320869d6e58bdade
Amantha96 <amanthabandara1996@gmail.com>
2023-11-10 00:18:22 +0530

    Start the image processing

--- src/components/MiddleBox/Middlebox.jsx
+++ src/components/MiddleBox/Middlebox.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './middlebox.css';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import { ref, uploadBytes } from "firebase/storage";
 import { v4 } from 'uuid';
 import { imageDB } from '../../firebase/config';
 import CircularProgress from '@mui/material/CircularProgress';
+import * as tf from '@tensorflow/tfjs';
+import * as cocossd from '@tensorflow-models/coco-ssd';
 
 function MiddleBox() {
     const [img, setImg] = useState(null);
@@ -12,31 +14,49 @@ function MiddleBox() {
     const fileInputRef = useRef(null);
 
     const [buttonText, setButtonText] = useState("UPLOAD");
+    const [model, setModel] = useState(null);
 
     // State to toggle whether the yellowbox image is displayed
     const [isYellowboxImageDisplayed, setIsYellowboxImageDisplayed] = useState(false);
 
-    const handleUpload = () => {
+    const handleUpload = async () => {
         if (img) {
             setIsLoading(true);
             const imgRef = ref(imageDB, `files/${v4()}`);
-            uploadBytes(imgRef, img)
-                .then(() => {
-                    alert('Image uploaded successfully');
-                    setImg(null);
-                    setButtonText("TRACK");
-                    setIsYellowboxImageDisplayed(false); // Reset the yellowbox image display
-                    setIsLoading(false);
-                })
-                .catch((error) => {
-                    console.error('Error uploading image:', error);
-                    setIsLoading(false);
-                });
+            try {
+                await uploadBytes(imgRef, img);
+                alert('Image uploaded successfully');
+                setImg(null);
+                setButtonText("TRACK");
+                setIsYellowboxImageDisplayed(false);
+                setIsLoading(false);
+
+                if (model) {
+                    // Perform object detection and render bounding boxes
+                    await performObjectDetection(img);
+                } else {
+                    console.error('Model is not loaded.');
+                }
+            } catch (error) {
+                console.error('Error uploading image:', error);
+                setIsLoading(false);
+            }
         } else {
             console.error('No image selected for upload');
         }
-    }
+    };
 
+    useEffect(() => {
+        const loadModel = async () => {
+            try {
+                const loadedModel = await cocossd.load();
+                setModel(loadedModel);
+            } catch (error) {
+                console.error('Error loading the model:', error);
+            }
+        };
+        loadModel();
+    }, []);
 
     const handleFileChange = (event) => {
         const selectedImage = event.target.files[0];
@@ -46,7 +66,7 @@ function MiddleBox() {
             const yellowbox = document.querySelector('.yellowbox');
             yellowbox.style.backgroundImage = `url(${URL.createObjectURL(selectedImage)}`;
             yellowbox.style.backgroundSize = 'cover';
-            setIsYellowboxImageDisplayed(true); // Set the yellowbox image display to true
+            setIsYellowboxImageDisplayed(true);
         }
     }
 
@@ -54,11 +74,44 @@ function MiddleBox() {
         fileInputRef.current.click();
     }
 
-    // Function to reset the yellowbox image
     const resetYellowbox = () => {
         const yellowbox = document.querySelector('.yellowbox');
         yellowbox.style.backgroundImage = 'none';
-        setIsYellowboxImageDisplayed(false); // Reset the yellowbox image display
+        setIsYellowboxImageDisplayed(false);
+    }
+
+    // Function to perform object detection and render bounding boxes
+    const performObjectDetection = async (image) => {
+        const imageElement = document.createElement('img');
+        imageElement.src = URL.createObjectURL(image);
+        await imageElement.decode();
+
+        const predictions = await model.detect(imageElement);
+        renderBoundingBoxes(predictions);
+    }
+
+    // Function to render bounding boxes
+    const renderBoundingBoxes = (predictions) => {
+        const yellowbox = document.querySelector('.yellowbox');
+        const imageElement = document.createElement('img');
+        imageElement.src = yellowbox.style.backgroundImage.replace(/url\(['"]?([^'"]*)['"]?\)/g, '$1');
+        const scaleFactorX = imageElement.width / yellowbox.offsetWidth;
+        const scaleFactorY = imageElement.height / yellowbox.offsetHeight;
+
+        // Create a canvas on top of the image
+        const canvas = document.createElement('canvas');
+        canvas.width = yellowbox.offsetWidth;
+        canvas.height = yellowbox.offsetHeight;
+        yellowbox.appendChild(canvas);
+
+        const ctx = canvas.getContext('2d');
+
+        predictions.forEach((prediction) => {
+            const bbox = prediction.bbox;
+            ctx.strokeStyle = 'red'; // Box color
+            ctx.lineWidth = 2;
+            ctx.strokeRect(bbox[0] * scaleFactorX, bbox[1] * scaleFactorY, bbox[2] * scaleFactorX, bbox[3] * scaleFactorY);
+        });
     }
 
     return (
